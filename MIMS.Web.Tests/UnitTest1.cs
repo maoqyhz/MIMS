3b035f78a68463baa1b58f6017a5e990db7af320
QingyuMao <maoqyzj@outlook.com>
2016-03-30 14:59:23 +0800

    构建整个毕设的三层架构，添加Easyui、Dapper dot net等必须的nuget包。

---
+++ MIMS.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MIMS.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
